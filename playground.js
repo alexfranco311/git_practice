0fcf56c9dda84a75605bcce077f70dcd519fc216
alexfranco311 <alexfranco311@gmail.com>
2021-09-04 22:22:57 -0400

    iterator notes

--- playground.js
+++ playground.js
@@ -222,3 +222,116 @@ git push origin <branch_name>: Pushes a local branch to the origin remote.
 */
 //9-1 SEPTEMBER!!!! back to javascript course
 //Sleep Debt Calculator, create a new document using terminal, 
+//9-2 Scopes,Arrays
+/*
+.length when logging an array will show you the total ammount of variables within the array
+  const objectives = ['Learn a new languages', 'Read 52 books', 'Run a marathon'];
+  console.log(objectives.length)
+  //output will be 3
+.push() will add an item to the end of an array
+.pop() will remove the last item of an array
+LOOPS
+for loop
+  const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+  for (let i = 0; i < vacationSpots.length; i++){
+    console.log('I would love to visit ' + vacationSpots[i])};
+*/
+//9-4 Iterators
+/*
+
+.forEach() is used to execute the same code on every element in an array but does not change the array and returns undefined.
+.map() executes the same code on every element in an array and returns a new array with the updated elements.
+.filter() checks every element in an array to see if it meets certain criteria and returns a new array with the elements that return truthy for the criteria.
+.findIndex() returns the index of the first element of an array which satisfies a condition in the callback function. It returns -1 if none of the elements in the array satisfies the condition.
+.reduce() iterates through an array and takes the values of the elements and returns a single value.
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter(num => {
+  return num < 250; 
+  });
+console.log(smallNumbers);
+
+break;
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+// Call .filter() on favoriteWords below
+const longFavoriteWords = favoriteWords.filter(word => {
+  return word.length >7
+});
+console.log(longFavoriteWords)
+
+break;
+
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+const secretMessage = animals.map(animal => animal[0]);
+  
+console.log(secretMessage.join(''));
+
+break;
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(number => number/100);
+
+console.log(smallNumbers);
+
+break;
+
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+
+const foundAnimal = animals.findIndex(animal => {
+  return animal === 'elephant';
+});
+console.log(foundAnimal)
+
+const startsWithS = animals.findIndex(animal => {
+  return animal[0] === 's' ? true : false;
+});
+
+console.log(startsWithS)
+
+break;
+
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue
+}, 10)
+console.log(newSum);
+
+break;
+fill in the correct method that will correct the errors:
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+// Choose a method that will return a new array
+const longCities = cities.filter(city => city.length > 7);
+
+// Choose a method that will return a single value
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0]
+}, "C");
+
+console.log(word)
+
+// Choose a method that will return a new array
+const smallerNums = nums.map(num => num - 5);
+
+// Choose a method that will return a boolean value
+nums.every(num => num < 0);
+
+break;
+
+*/
