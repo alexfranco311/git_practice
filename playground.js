b876b16f84f7f9e1847ded3eab09eef1c97e635b
alexfranco311 <alexfranco311@gmail.com>
2021-09-07 23:02:41 -0400

    Start of front end carrer path, HTML notes

--- playground.js
+++ playground.js
@@ -456,4 +456,16 @@ in terminal
 4.add the scripts to package.json, under the "test", dont forget the comma.
     "build": "babel src -d lib"
 5. npm run build
+
+HTML
+-HTML stands for HyperText Markup Language and is used to create the structure and content of a webpage.
+-Most HTML elements contain opening and closing tags with raw text or other HTML tags between them.
+-HTML elements can be nested inside other elements. The enclosed element is the child of the enclosing parent element.
+-Any visible content should be placed within the opening and closing <body> tags.
+-Headings and sub-headings, <h1> to <h6> tags, are used to provide titles for sections of content.
+-<p>, <span> and <div> tags specify text or blocks.
+-The <em> and <strong> tags are used to emphasize text.
+-Line breaks are created with the <br> tag.
+-Ordered lists (<ol>) are numbered and unordered lists (<ul>) are bulleted.
+-Images (<img>) and videos (<video>) can be added by linking to an existing source.
 */
