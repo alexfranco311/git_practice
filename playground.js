4c2e0749e4095bffbfa624d57ed4548a682b7412
alexfranco311 <alexfranco311@gmail.com>
2021-09-09 00:19:28 -0400

    slight modification to html doc and new notes on html tables

--- playground.js
+++ playground.js
@@ -468,4 +468,33 @@ HTML
 -Line breaks are created with the <br> tag.
 -Ordered lists (<ol>) are numbered and unordered lists (<ul>) are bulleted.
 -Images (<img>) and videos (<video>) can be added by linking to an existing source.
+
+created the webpage.html document using the terminal. commit, merge push to git to continue the consistent practice of git
+*/
+/* 9/8 more html
+-The <!DOCTYPE html> declaration should always be the first line of code in your HTML files. This lets the browser know what 
+version of HTML to expect.
+-The <html> element will contain all of your HTML code.
+-Information about the web page, like the title, belongs within the <head> of the page.
+-You can add a title to your web page by using the <title> element, inside of the head.
+-A webpage’s title appears in a browser’s tab.
+-Anchor tags (<a>) are used to link to internal pages, external pages or content on the same page.
+-You can create sections on a webpage and jump to them using <a> tags and adding ids to the elements you wish to jump to.
+-Whitespace between HTML elements helps make code easier to read while not changing how elements appear in the browser.
+-Indentation also helps make code easier to read. It makes parent-child relationships visible.
+-Comments are written in HTML using the following syntax: <!-- comment -->.
+*/
+console.log("Today is Sept. 9. Front end dev course is at 9%");
+/* 9/9 html Tables!
+-The <table> element creates a table.
+-The <tr> element adds rows to a table.
+-To add data to a row, you can use the <td> element.
+-Table headings clarify the meaning of data. Headings are added with the <th> element.
+-Table data can span columns using the colspan attribute.
+-Table data can span rows using the rowspan attribute.
+-Tables can be split into three main sections: a head, a body, and a footer.
+-A table’s head is created with the <thead> element.
+-A table’s body is created with the <tbody> element.
+-A table’s footer is created with the <tfoot> element.
+-All the CSS properties you learned about in this course can be applied to tables and their data.
 */
