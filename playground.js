08a1fd801053e461c26e1d84d0dadbed1879930c
alexfranco311 <alexfranco311@gmail.com>
2021-09-15 00:30:28 -0400

    Notes: The Box Model - Flow of HTML

--- playground.js
+++ playground.js
@@ -576,3 +576,47 @@ You can add opacity to color in RGB and HSL by adding a fourth value, a, which i
 -Google Fonts provides free fonts that can be used in an HTML file with the <link> tag or the @font-face property at the top of the css file.
 -Local fonts can be added to a document with the @font-face property and the path to the font’s source.
 */
+/* 9/14 The Box Model
+-width and height: The width and height of the content area.
+-padding: The amount of space between the content area and the border.
+-border: The thickness and style of the border surrounding the content area and padding.
+-margin: The amount of space between the border and the outside edge of the element.
+
+Restting default values for the css rules accross different browsers
+    * {
+      margin: 0;
+      padding: 0;
+    }
+  at the top of the CSS document for the html document in question.
+
+Review of The Box Model
+-The box model comprises a set of properties used to create space around and between HTML elements.
+-The height and width of a content area can be set in pixels or percentages.
+-Borders surround the content area and padding of an element. The color, style, and thickness of a border can be set with CSS properties.
+-Padding is the space between the content area and the border. It can be set in pixels or percent.
+-Margin is the amount of spacing outside of an element’s border.
+-Horizontal margins add, so the total space between the borders of adjacent elements is equal to the sum of the right margin of one element
+   and the left margin of the adjacent element.
+-Vertical margins collapse, so the space between vertically adjacent elements is equal to the larger margin.
+-margin: 0 auto horizontally centers an element inside of its parent content area, if it has a width.
+-The overflow property can be set to display, hide, or scroll, and dictates how HTML will render content that overflows its parent’s content area.
+-The visibility property can hide or show elements.
+
+Changing the box model
+    In the default box model, box dimensions are affected by border thickness and padding.
+    The box-sizing property controls the box model used by the browser.
+    The default value of the box-sizing property is content-box.
+      * {
+        box-sizing: border-box
+      }
+    The value for the new box model is border-box.
+    The border-box model is not affected by border thickness or padding.
+
+Flow of HTML
+  Position- The position property can take one of five values:
+    -static - the default value (it does not need to be specified)
+    -relative
+    -absolute
+    -fixed
+    -sticky
+*/
